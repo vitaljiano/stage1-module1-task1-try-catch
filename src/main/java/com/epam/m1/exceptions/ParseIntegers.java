7839802570045e789fe06eda84ac615290e53e78
vhorbatiuk <vhorbatiuk@binariks.com>
2024-03-26 10:25:25 +0200

    Add Logger info

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -3,13 +3,15 @@ package com.epam.m1.exceptions;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Complete the code, parse integers, calculate the sum of numbers in the WORDS, join strings with
  * space delimiter
  */
 public class ParseIntegers {
-
+static Logger LOGGER;
     private static final List<String> WORDS =
             Arrays.asList(
                     "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
@@ -25,8 +27,8 @@ public class ParseIntegers {
             sum = sum + number;
             justWords = justWords.concat(" " + next);
         }
-        System.out.println("Sum is " + sum);
-        System.out.println("Just words:" + justWords);
+        LOGGER.log(Level.INFO,"Sum is " + sum);
+        LOGGER.log(Level.INFO,"Just words:" + justWords);
     }
 }
 
